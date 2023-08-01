05aafad05fc76c8d12fdb85250dec2db185144b1
Jezelle23 <jezerealgilbert@gmail.com>
2023-08-01 06:20:24 -0700

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,7 +27,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * Return: 0 (if tree is NULL)
  *
  */
-int binary_tree_balance(const binary_tree_t* tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	if (tree)
 	{
