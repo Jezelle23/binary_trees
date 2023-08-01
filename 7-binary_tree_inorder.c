553be3c84ae235b53c5eb1f433af45e6c73be71a
Jezelle23 <jezerealgilbert@gmail.com>
2023-08-01 05:44:45 -0700

    7-binary_tree_inorder.c

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_inorder - displays elements of a tree in order
+ *@tree: Pointer to the root node
+ *@func: Pointer to a function
+ * 
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
